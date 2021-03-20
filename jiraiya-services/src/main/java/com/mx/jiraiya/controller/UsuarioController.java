a941d8d6b10f0a2a9841b5214dc6afc778c9c3df
eltigueeere <laloeltigueeere@gmail.com>
2021-03-20 16:11:12 -0600

    lalos

--- jiraiya-services/src/main/java/com/mx/jiraiya/controller/UsuarioController.java
+++ jiraiya-services/src/main/java/com/mx/jiraiya/controller/UsuarioController.java
@@ -7,14 +7,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-@CrossOrigin(origins = ("http://127.0.0.1:8080"))
-@RestController
+@Controller
 public class UsuarioController {
 
     Logger logger = LogManager.getLogger(UsuarioController.class);
@@ -23,11 +22,18 @@ public class UsuarioController {
     @Autowired
     private CrudUsuario _CrudUsuario = new CrudUsuario();
 
+    @Secured("ROLE_USER")
     @PostMapping("/resetPassword")
     public ResponseEntity<?> updatePassword(@RequestBody Usuario usuario)  {
         logger.info("Cabio de contraseña.");
         return _CrudUsuario.updatePasswordService(usuario);
     }
+
+    @GetMapping("/hakuLogin")
+    public String hakuLogin() {
+        return "view/login";
+    }
+    
 
 }
 
